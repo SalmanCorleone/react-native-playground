f2ec3b8293dfbe1a118e4cb0e0705af39ba7bd0f
Salman <samilsalman12@gmail.com>
2020-02-16 06:15:57 +0600

    Fix navigation

--- src/config/navigation.js
+++ src/config/navigation.js
@@ -3,12 +3,32 @@ import TaskList from '../screens/TaskList';
 import DoneList from '../screens/DoneList';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import {Icon} from 'react-native-elements';
 
 const Tab = createBottomTabNavigator();
 
 const AppWithNavigation = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Tasks" component={TaskList} options={} />
+  <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Tasks') {
+          iconName = focused ? 'pluscircle' : 'pluscircleo';
+        } else if (route.name === 'Completed') {
+          iconName = focused ? 'pluscircle' : 'pluscircleo';
+        }
+
+        return (
+          <Icon type="antdesign" name={iconName} size={size} color={color} />
+        );
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    }}>
+    <Tab.Screen name="Tasks" component={TaskList} />
     <Tab.Screen name="Completed" component={DoneList} />
   </Tab.Navigator>
 );
